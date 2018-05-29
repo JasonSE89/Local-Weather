420c5c9e1f93428e882ba06fda493b9ddf512cff
Jason <jason.epstein89@gmail.com>
2018-05-29 00:19:06 -0400

    Application now includes the city, country, and temperature of the user

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //get geograpchic coordinates of user
 function localCoordinates(callback){
-  var thisthing;
   navigator.geolocation.getCurrentPosition(function(position) {
 
     latitude = position.coords.latitude;
@@ -10,17 +9,20 @@ function localCoordinates(callback){
   );
   }
 
-  function oliver(a)
-  {
-    console.log(a)
-  }
+//Formats data for index.html page
+function formatData(city, country, temperature){
+  $(".container").append("<h3 class="+ "location"+">"+city+", "+country+"</h3>");
+  $(".container").append("<p>"+temperature+"</p>");
+}
 
 //get weather based on coordinates
 function getWeather(latCor, longCor){
   $.getJSON("https://fcc-weather-api.glitch.me/api/current?"+"lat="+latCor+"&"+"lon="+longCor, function(weatherData){
+    formatData(weatherData.name, weatherData.sys.country, weatherData.main.temp);
     console.log(weatherData);
   });
 }
+
 
 
 
