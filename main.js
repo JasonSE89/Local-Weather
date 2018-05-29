f3174d7fbf5bff0d71b8ff9eeabadd0b014a60f1
Jason <jason.epstein89@gmail.com>
2018-05-28 20:54:34 -0400

    main.js file updated, able to gather weather based on user's location

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-//get location of user
-function userlocation(){
+//get geograpchic coordinates of user
+function localCoordinates(callback){
+  var thisthing;
   navigator.geolocation.getCurrentPosition(function(position) {
-  console.log(position.coords.latitude, position.coords.longitude);
-});
 
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    callback(latitude, longitude);
+  }
+  );
+  }
+
+  function oliver(a)
+  {
+    console.log(a)
+  }
+
+//get weather based on coordinates
+function getWeather(latCor, longCor){
+  $.getJSON("https://fcc-weather-api.glitch.me/api/current?"+"lat="+latCor+"&"+"lon="+longCor, function(weatherData){
+    console.log(weatherData);
+  });
 }
 
+
+
 $(document).ready(function(){
-  userlocation();
+  localCoordinates(function(lattitude, longitude){
+    getWeather(lattitude, longitude);
+  });
 });
